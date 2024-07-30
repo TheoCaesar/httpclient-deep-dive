9d0d4e10c7eadd2d27f8167cc7e4edfb1157a29c
TheoCaesar <theocaesar7@gmail.com>
2024-07-30 20:12:09 +0100

    transform errors with catchError rxjs operator

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -27,15 +27,18 @@ export class AvailablePlacesComponent implements OnInit {
       get<{places: Place[]}>('http://localhost:3000/places', {
         observe:'response' //or events as a value;
       }).pipe(
-        map((data) => data.body?.places)
+        map((data) => data.body?.places),
+        catchError((error_)=> {
+          console.log(error_)
+          return throwError(()=>new Error("Something went wrong! Please try again later..."))
+        })
       ).subscribe({
         next:(response)=>{ 
           console.log(response)
           this.places.set(response)
         },
-        error: (err) => {
-          console.log(err.name, err.message)
-          this.isError.set(`${err.statusText}`)
+        error: (err: Error) => {
+          this.isError.set(`${err.message}`)
           this.isLoading.set(undefined);
         },
         complete: ()=> this.isLoading.update((loading)=> loading = false)
